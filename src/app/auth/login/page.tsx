1537efc59779877307f68c88b1f29a31a9c11ab1
nataliabaez96 <nataliabaez96@gmail.com>
2026-01-14 00:20:37 -0300

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,21 +1,3 @@
-import { Grid, Paper } from '@mui/material';
-import HankoAuth from '../../components/HankoAuth';
-
 export default function LoginPage() {
-    return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ minHeight: '100vh' }}
-        >
-            <Grid item xs={3}>
-                <Paper sx={{ p: 2 }}>
-                    <HankoAuth />
-                </Paper>
-            </Grid>
-        </Grid>
-    );
+  return <div>LOGIN TESTE</div>
 }
